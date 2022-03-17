66188f658e2f9384ea9a95eaec938f3732d15726
KimJinYoung <zsktnflahs13@gmail.com>
2022-03-17 10:18:16 +0900

    controller area replace

--- src/main/java/com/devjin/springstu/controller/getstuController.java
+++ src/main/java/com/devjin/springstu/controller/getstuController.java
@@ -1,4 +1,4 @@
-package com.devjin.springstu;
+package com.devjin.springstu.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
